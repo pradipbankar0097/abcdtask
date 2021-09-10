3f7e95ee16e9bdca0bd1ee66077bb91e3e926615
pradipbankar0097 <pradipbankar0097@gmail.com>
2021-09-10 13:50:16 +0530

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, TextInput, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+const allItems = [
+  "Milk",
+  "Coffee",
+  "Oranges",
+  "Bread",
+];
+
 
 export default function App() {
+
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  const results = !searchTerm
+    ? allItems
+    : allItems.filter(item =>
+        item.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+      );
+
   return (
+    <View>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={{height: 40,}}
+        placeholder="Type here to translate!"
+        onChangeText={searchTerm=>setSearchTerm(searchTerm)}
+        defaultValue={searchTerm}
+      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={()=>{allItems.push(searchTerm),setSearchTerm('')}}
+      >
+        <Text>+</Text>
+      </TouchableOpacity>
+      
     </View>
+    <Text style={{padding: 10, fontSize: 42}}>
+    {results.map(item => (
+        <View style={{
+          display:'flex',
+          flexDirection:'column',
+          padding:10,
+          backgroundColor:'rgb('
+        }}>
+          <Text>{item}</Text>
+        </View>
+        ))}
+  </Text>
+  </View>
   );
 }
 
@@ -15,7 +57,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display:'flex',
+    flexDirection: 'row',
   },
+  button: {
+    backgroundColor:'blue',
+    padding: 15,
+    borderRadius:7,
+  }
 });
